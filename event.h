03ce6b677d71d867e8711276520fd15b5aaa36b5
Alysia <kroykie@gmail.com>
2018-02-18 22:37:48 -0800

    Update event.h

--- event.h
+++ event.h
@@ -6,12 +6,23 @@ class Event
 public:
 
 private:
+  double arrival_time;
+  double service_time;
+  char event_type;
+  Event *next_event;
+  Event *previous_event;
 }
 
 class Arrival: public Event
 {
+public:
+  Arrival( char ev_type = 'a', double arr_time, double ser_time );
 }
 
 class Departure: public Event
 {
+public:
+  Departure( char ev_type = 'd', double dep_time );
 }
+
+#endif
